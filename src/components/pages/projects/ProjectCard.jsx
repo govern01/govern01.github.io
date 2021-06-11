51e8351e31e7876854b31273cf8d32a2d32b9a50
Jacob Morris <govern01mail@gmail.com>
2021-06-12 00:29:21 +1000

    Added SCSS to Projects segment.
    Site is now in a pretty usable stage, I can start doing better data entry and focus on SEO.

--- src/components/pages/projects/ProjectCard.jsx
+++ src/components/pages/projects/ProjectCard.jsx
@@ -3,7 +3,7 @@ const ProjectCard = ({ data }) => {
         <div className="card_container">
             <article className="card">
                 <header>
-                    <h3>{data.title}</h3><h4>- {data.date}</h4>
+                    <h3>{data.title}</h3><h4> - {data.date}</h4>
                     <p>{data.tagline}
                     &nbsp;--&nbsp;
                     {data.opensource ? <a href={data.source} rel="norefference">Source here</a> : "Closed Source"}</p>
@@ -11,14 +11,18 @@ const ProjectCard = ({ data }) => {
                 {/* I'll add in proper images once I find good images for each project */}
                 {/* Which means I really need to find the source for that anti-theft */}
                 {/* For now we shall use tasteful images of dogs */}
-                <img src={`${process.env.PUBLIC_URL}${data.img}`} alt={`${data.title}-img not found`} />
-                <div>
+                <div className="image">
+                    <img src={`${process.env.PUBLIC_URL}${data.img}`} alt={`${data.title}-img not found`} />
+                </div>
+                <div className="description">
                     <p>{data.desc}</p>
-                    <ul>
-                        {data.technologies.map((tech) => (
-                            <li key={tech}>{tech}</li>
-                        ))}
-                    </ul>
+                    <div className="techlist">
+                        <ul>
+                            {data.technologies.map((tech) => (
+                                <li key={tech}>{tech}</li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </article>
         </div>
